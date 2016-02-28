9b6d261fda69f751ea0d6940d4b8d0a952040d05
stern15 <ssemasuka@gmail.com>
2016-02-28 15:56:49 +0300

    adding url to the project

--- inventory/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.db import models
-
-
-class Item(models.Model):
-	title = models.CharField(max_lenght=200)
-	description = models.TextField()
-	amount = models.IntegerField()
-	
